369b338c4cca577e7e85f63ec95cf07b32b3bfbc
zaknryan <48138615+zaknryan@users.noreply.github.com>
2023-09-26 14:07:10 -0400

    Update mortgageComparison.js

--- mortgageComparison.js
+++ mortgageComparison.js
@@ -53,7 +53,51 @@ function mortgageComparison(p1, marketRates, marketRateLender = 0.03) {
     return JSON.stringify(output);
 }
 
-// ... (rest of the code remains unchanged)
+function loanPaymentDetails(principal, annualInterestRate, amortPeriods, periods) {
+    const monthlyInterestRate = periodInterestRate(annualInterestRate, 12);
+
+    const monthlyPayment = Math.round(principal * (monthlyInterestRate * Math.pow(1 + monthlyInterestRate, amortPeriods)) / (Math.pow(1 + monthlyInterestRate, amortPeriods) - 1)*100)/100;
+
+    let totalPayments = 0;
+    let totalPrincipalPayments = 0;
+    let totalInterestPayments = 0;
+    let remainingBalance = principal;
+
+    for (let i = 0; i < periods; i++) {
+        const interestPayment = remainingBalance * monthlyInterestRate;
+        const principalPayment = monthlyPayment - interestPayment;
+
+        remainingBalance -= principalPayment;
+
+        totalPayments += monthlyPayment;
+        totalPrincipalPayments += principalPayment;
+        totalInterestPayments += interestPayment;
+    }
+
+    return {
+        totalPayments: Math.round(totalPayments * 100) / 100,
+        totalPrincipalPayments: Math.round(totalPrincipalPayments * 100) / 100,
+        totalInterestPayments: Math.round(totalInterestPayments * 100) / 100,
+        endingPrincipal: Math.round(remainingBalance * 100) / 100,
+        monthlyPayment: monthlyPayment,
+        interestRate: annualInterestRate
+    };
+}
+
+function periodInterestRate(annRate, periodsPerYear, compoundingPeriodsPerYear = 2) {
+    return Math.pow(1 + annRate / compoundingPeriodsPerYear, compoundingPeriodsPerYear / periodsPerYear) - 1;
+}
+
+function calculateBreakFees(remainingPrinciple, remainingPeriods, mortgageRate, currentRate) {
+    const IRD = mortgageRate - currentRate;
+    const IRDPeriod = remainingPrinciple * (IRD / 12);
+
+    const IRDOwing = IRDPeriod * remainingPeriods;
+
+    const breakFees = Math.max(IRDOwing, 3 * mortgageRate / 12 * remainingPrinciple);
+
+    return breakFees;
+}
 
 mortgageDetails = p1;
 marketRates = p2;
