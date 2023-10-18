b57aa5dfd1755a43e5971ca5165a4c1e1b399ab2
zaknryan <48138615+zaknryan@users.noreply.github.com>
2023-10-18 10:50:31 -0400

    Update qualified_market_rates.js

--- qualified_market_rates.js
+++ qualified_market_rates.js
@@ -1,18 +1,18 @@
 function filterRates(currentMortgage, maturityDate, marketRates) {
-  // If interest type is not 'Fixed', return null
-  if (currentMortgage.interestTypeDd !== 'Fixed') {
-    return null;
-  }
 
-  const currentRate = currentMortgage.requestedRate;
+const currentRate = currentMortgage.requestedRate;
   
-  // Process and filter marketRates
-  marketRates = marketRates.map(rate => {
-    const match = rate.Term.match(/^(\d+) YR$/);
-    rate.termYears = match ? parseInt(match[1]) : null;
-    rate.Rate = parseFloat(rate.Rate);
-    return rate;
-  }).filter(rate => rate.termYears !== null && !isNaN(rate.Rate) && rate.Rate < currentRate);
+// Process and filter marketRates
+marketRates = marketRates.map(rate => {
+  const match = rate.Term.match(/^(\d+) YR( VRM)?$/);
+  if (match) {
+    rate.termYears = parseInt(match[1]);
+    rate.type = match[2] ? "Variable" : "Fixed";
+  }
+  rate.Rate = parseFloat(rate.Rate);
+  rate.Lender = rate.Lender.replace(/\n/g, '');
+  return rate;
+}).filter(rate => rate.termYears !== undefined && !isNaN(rate.Rate) && rate.Rate < currentRate);
 
   // Filter out rates that are closest to year_to_maturity 
   return marketRates;
