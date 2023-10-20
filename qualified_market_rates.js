0c06edf86816eb50e3eb2a2644db246ee7c2d830
zaknryan <48138615+zaknryan@users.noreply.github.com>
2023-10-20 16:47:54 -0400

    Update qualified_market_rates.js
    Changes to accept prime rate in the case of variable rates

--- qualified_market_rates.js
+++ qualified_market_rates.js
@@ -1,26 +1,32 @@
-function filterRates(currentMortgage, maturityDate, marketRates) {
+function filterRates(currentMortgage, marketRates, currentPrimeRate) {
 
-const currentRate = currentMortgage.requestedRate;
-  
-// Process and filter marketRates
-marketRates = marketRates.map(rate => {
-  const match = rate.Term.match(/^(\d+) YR( VRM)?$/);
-  if (match) {
-    rate.termYears = parseInt(match[1]);
-    rate.type = match[2] ? "Variable" : "Fixed";
+  if currentMortgage.interestTypeDd == 'Fixed' {
+    const currentRate = currentMortgage.requestedRate;
+  } else {
+    const currentRate = currentPrimeRate - currentMortgage.rate.discount + currentMortgage.rate.premium
   }
-  rate.Rate = parseFloat(rate.Rate);
-  rate.Lender = rate.Lender.replace(/\n/g, ' ');
-  return rate;
-}).filter(rate => rate.termYears !== undefined && !isNaN(rate.Rate) && rate.Rate < currentRate);
-
-  // Filter out rates that are closest to year_to_maturity 
-  return marketRates;
-}
-
-// Parse and call the function
-const marketRates = JSON.parse('[' + p3 + ']');
-const currentMortgage = JSON.parse(p1);
-const maturityDate = p2;
-
-return JSON.stringify(filterRates(currentMortgage, maturityDate,marketRates));
+  
+  const maturityDate = currentMortgage.maturityDate;
+    
+  // Process and filter marketRates
+  marketRates = marketRates.map(rate => {
+    const match = rate.Term.match(/^(\d+) YR( VRM)?$/);
+    if (match) {
+      rate.termYears = parseInt(match[1]);
+      rate.type = match[2] ? "Variable" : "Fixed";
+    }
+    rate.Rate = parseFloat(rate.Rate);
+    rate.Lender = rate.Lender.replace(/\n/g, ' ');
+    return rate;
+  }).filter(rate => rate.termYears !== undefined && !isNaN(rate.Rate) && rate.Rate < currentRate);
+  
+    // Filter out rates that are closest to year_to_maturity 
+    return marketRates;
+  }
+  
+  // Parse and call the function
+  const marketRates = JSON.parse('[' + p3 + ']');
+  const currentMortgage = JSON.parse(p1);
+  const currentPrimeRate = p2;
+  
+  return JSON.stringify(filterRates(currentMortgage, marketRates, currentPrimeRate));
