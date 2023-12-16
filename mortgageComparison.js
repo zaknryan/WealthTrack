717ac6ce3cbe1ca7b40059a6a8d9db1201add264
Ryan Krook <ryan@Ryans-MacBook-Pro.local>
2023-12-15 23:26:58 -0500

    Adding gds, tds and ltv calculations

--- mortgageComparison.js
+++ mortgageComparison.js
@@ -2,9 +2,11 @@ function mortgageComparison(existingMortgage, marketRates, marketPrime, marketRa
     // Set variables from properties in currentMortgage
     const {
         amortizationTerm,
+        actualPaymentTerm: mortgageTerm,
         requestedRate: currentMortgageRate,
         interestTypeDd: interestType,
         balanceRemaining,
+        netLoanAmount,
         rate: {
             discount: rateDiscount = 0,   // Default value for discount is 0
             premium: ratePremium = 0      // Default value for premium is 0
@@ -13,6 +15,7 @@ function mortgageComparison(existingMortgage, marketRates, marketPrime, marketRa
         combinedIncome,
         qualifyingGds,
         qualifyingTds,
+        PAndIPaymentAmountMonthly: currentPayment
     } = existingMortgage
 
     // Set calculated variables based on currentMortgage
@@ -24,59 +27,86 @@ function mortgageComparison(existingMortgage, marketRates, marketPrime, marketRa
     startDate.setMonth(startDate.getMonth() - 1); // Set month one month prior
 
     // Calculate periodsRemaining
-    let today = new Date();
-    let currentPeriodsRemaining = monthDiff(today, maturityDate);
+    //let today = new Date();
+    //let currentPeriodsRemaining = monthDiff(today, maturityDate);
+
+    // Estimate propTax_Heat = propTax + Heating = HouseExp - Mortgage from GSD and TSD
+    const houseExpenses = qualifyingGds * (combinedIncome);
+    const propTax_Heat = houseExpenses - currentPayment*12;
+
+    const otherExp = (combinedIncome) * (qualifyingTds - qualifyingGds);
 
     // Calculate amortPeriodsRemaining
-    let amortPeriodsRemaining = amortizationTerm - monthDiff(startDate, today);
+    let today = new Date();
+    let periodsPast = monthDiff(startDate, today);
     // Ensure amortPeriodsRemaining is not negative
-    amortPeriodsRemaining = amortPeriodsRemaining <= 0 ? 0 : amortPeriodsRemaining;
+    //amortPeriodsRemaining = amortPeriodsRemaining <= 0 ? 0 : amortPeriodsRemaining;
 
     // Calculate the current rate used to determine payments
     let currentRate = interestType == 'Fixed' ? currentMortgageRate : marketPrime - rateDiscount + ratePremium;
 
+    // Calculate propertyValue based on ltv and original loan amount
+    let propertyValue = netLoanAmount / ltv;
+
     // Get loan payment details for current Mortgage
-    const currentMortgage = loanPaymentDetails(
-        balanceRemaining,
+    const currentMortgage = mortgageDetails(
+        netLoanAmount,
         currentRate / 100,
-        amortPeriodsRemaining,
-        currentPeriodsRemaining
+        amortizationTerm,
+        mortgageTerm,
+        periodsPast,
+        propertyValue,
+        combinedIncome,
+        propTax_Heat,
+        otherExp
     );
     currentMortgage.type = interestType;
 
     const estBreakFees = calculateBreakFees(
         balanceRemaining,
-        currentPeriodsRemaining,
+        mortgageTerm - periodsPast,
         currentRate / 100, // <-- Corrected rate variable
         marketRateLender / 100
     );
 
     //Run through each of the market rates and calculate the mortgage details
     const marketMortgages = marketRates.map(rateObj => {
-        const marketLoanDetails = loanPaymentDetails(
-            balanceRemaining,
-            rateObj.Rate/100,
-            amortPeriodsRemaining,
-            rateObj.termYears * 12, currentPeriodsRemaining,
+        const marketLoanDetails = mortgageDetails(
+            currentMortgage.principalBalance,
+            rateObj.Rate / 100,
+            amortizationTerm - periodsPast,
+            rateObj.termYears * 12,
+            0,
+            propertyValue,        
+            combinedIncome,
+            propTax_Heat,
+            otherExp
         );
 
         let ix = Math.min(marketLoanDetails.cumulative.InterestPayments.length, currentMortgage.cumulative.InterestPayments.length) - 1;
-    
+
+        // Evaluate GDS, TDS and LTV ratios
+        // ratioLimits are given in whole numbers ie 80, rather than 0.8
+        marketLoanDetails.ratios.gdsQualified = marketLoanDetails.ratios.gds < rateObj.ratioLimits.gds/100;
+        marketLoanDetails.ratios.tdsQualified = marketLoanDetails.ratios.tds < rateObj.ratioLimits.tds/100;
+        marketLoanDetails.ratios.ltvQualified = marketLoanDetails.ratios.ltv < rateObj.ratioLimits.ltv/100;
+        // Evaluate if a user qualifies under ALL ratios
+        marketLoanDetails.ratios.qualified = marketLoanDetails.ratios.gdsQualified && marketLoanDetails.ratios.tdsQualified && marketLoanDetails.ratios.ltvQualified;
+
+
         return {
-            ...rateObj, 
+            ...rateObj,
             ...marketLoanDetails,
-            //remaining: {remainingYears: marketLoanDetails.termYears, 
-            //                remainingPeriods: marketLoanDetails.termYears * 12},
             interestSavings: currentMortgage.cumulative.InterestPayments[ix] - marketLoanDetails.cumulative.InterestPayments[ix],           //Interest savings from moving to this mortgage
             principalPaymentInc: marketLoanDetails.cumulative.PrincipalPayments[ix] - currentMortgage.cumulative.PrincipalPayments[ix],     //Incremental monthly principal payment
             principalDiff: marketLoanDetails.cumulative.endingPrincipal[ix] - currentMortgage.cumulative.endingPrincipal[ix],                         //Difference in remaining principal at the end of the current mortgage
             paymentDelta: marketLoanDetails.monthlyPayment - currentMortgage.monthlyPayment,                             //Change in monthly payment (+ive means paying more)
-            compare_periods: ix + 1
+            compare_periods: ix + 1,
         };
     });
 
     //Find which mortgage has the lowest totalPayments
-    lowest = getIndexesOfMinValues(marketMortgages, ['totalPayments','totalInterestPayments','endingPrincipal','monthlyPayment'])
+    lowest = getIndexesOfMinValues(marketMortgages, ['totalPayments', 'totalInterestPayments', 'endingPrincipal', 'monthlyPayment'])
 
     const output = {
         currentMortgage: currentMortgage,
@@ -88,41 +118,41 @@ function mortgageComparison(existingMortgage, marketRates, marketPrime, marketRa
     return JSON.stringify(output);
 }
 
-function loanPaymentDetails(principal, annualInterestRate, amortPeriods, periods) {
+function mortgageDetails(principal, annualInterestRate, amortizationTerm, mortgageTerm, periodsPast, propertyValue, combinedIncome, propTax_Heat, otherExp) {
     //amortPeriods - the number of periods overwhich this remaining balance will be amortized
     //periods - the number of periods in the mortage deal (ie 5 years with a 25 year amortization)
     const monthlyInterestRate = periodInterestRate(annualInterestRate, 12);
 
-    const monthlyPayment = Math.round(principal * (monthlyInterestRate * Math.pow(1 + monthlyInterestRate, amortPeriods)) / (Math.pow(1 + monthlyInterestRate, amortPeriods) - 1)*100)/100;
-    
+    const monthlyPayment = Math.round(principal * (monthlyInterestRate * Math.pow(1 + monthlyInterestRate, amortizationTerm)) / (Math.pow(1 + monthlyInterestRate, amortizationTerm) - 1) * 100) / 100;
+
     const today = new Date(); // Today's date
-    
+
     const termEndDate = new Date(today); // Copy today's date to termEndDate
-    termEndDate.setMonth(today.getMonth() + periods); // Add X months to termEndDate
-    
+    termEndDate.setMonth(today.getMonth() + mortgageTerm - periodsPast); // Add X months to termEndDate
+
     const amortEndDate = new Date(today); // Copy today's date to amortEndDate
-    amortEndDate.setMonth(today.getMonth() + amortPeriods); // Add Y months to amortEndDate
+    amortEndDate.setMonth(today.getMonth() + amortizationTerm - periodsPast); // Add Y months to amortEndDate
 
     let cumulativePayments = [];
     let cumulativePrincipalPayments = [];
     let cumulativeInterestPayments = [];
     let remainingBalances = []; // Array to keep track of the remaining balance at each iteration
     let remainingBalance = principal;
-    
+
     let cumulativeTotalPayments = 0;
     let cumulativeTotalPrincipalPayments = 0;
     let cumulativeTotalInterestPayments = 0;
-    
-    for (let i = 0; i < periods; i++) {
+
+    for (let i = 0; i < mortgageTerm; i++) {
         let interestPayment = remainingBalance * monthlyInterestRate;
         let principalPayment = monthlyPayment - interestPayment;
-    
+
         remainingBalance -= principalPayment;
-    
+
         cumulativeTotalPayments += monthlyPayment;
         cumulativeTotalPrincipalPayments += principalPayment;
         cumulativeTotalInterestPayments += interestPayment;
-    
+
         // Pushing cumulative sums into the arrays at each iteration
         cumulativePayments.push(cumulativeTotalPayments);
         cumulativePrincipalPayments.push(cumulativeTotalPrincipalPayments);
@@ -135,22 +165,34 @@ function loanPaymentDetails(principal, annualInterestRate, amortPeriods, periods
     cumulativeInterestPayments = cumulativeInterestPayments.map(amount => Math.round(amount * 100) / 100);
     remainingBalances = remainingBalances.map(amount => Math.round(amount * 100) / 100);
 
+    //Calculate ratios
+    principalBalance = remainingBalances[periodsPast]
+    const ltv = principalBalance / propertyValue;
+    const gds = (propTax_Heat + monthlyPayment*12) / combinedIncome;
+    const tds = gds + (otherExp / combinedIncome);
+
     return {
         totalPayments: cumulativePayments[cumulativePayments.length - 1],
         totalPrincipalPayments: cumulativePrincipalPayments[cumulativePrincipalPayments.length - 1],
         totalInterestPayments: cumulativeInterestPayments[cumulativeInterestPayments.length - 1],
         endingPrincipal: remainingBalances[remainingBalances.length - 1],
+        principalBalance: principalBalance,
         monthlyPayment: monthlyPayment,
         interestRate: annualInterestRate,
+        ratios: {
+            ltv: ltv,
+            gds: gds,
+            tds: tds
+        },
         term: {
             endDate: termEndDate.toISOString(),
-            remainingYears: Math.round(periods / 12 * 10) / 10,
-            remainingPeriods: periods
+            remainingYears: Math.round((mortgageTerm - periodsPast) / 12 * 10) / 10,
+            remainingPeriods: (mortgageTerm - periodsPast)
         },
         amortization: {
             endDate: amortEndDate.toISOString(),
-            remainingYears: Math.round(amortPeriods / 12 * 10) / 10,
-            remainingPeriods: amortPeriods
+            remainingYears: Math.round((amortizationTerm - periodsPast) / 12 * 10) / 10,
+            remainingPeriods: (amortizationTerm - periodsPast)
         },
         cumulative: {
             Payments: cumulativePayments,
@@ -163,7 +205,7 @@ function loanPaymentDetails(principal, annualInterestRate, amortPeriods, periods
 
 function periodInterestRate(annRate, periodsPerYear, compoundingPeriodsPerYear = 2) {
     //Calculate effective annual interest rate
-    const effectiveAnnRate = Math.pow(1 + annRate / compoundingPeriodsPerYear, compoundingPeriodsPerYear)-1;
+    const effectiveAnnRate = Math.pow(1 + annRate / compoundingPeriodsPerYear, compoundingPeriodsPerYear) - 1;
 
     return Math.pow(1 + effectiveAnnRate, 1 / periodsPerYear) - 1;
 }
@@ -182,13 +224,13 @@ function calculateBreakFees(remainingPrinciple, remainingPeriods, mortgageRate, 
 function getIndexesOfMinValues(arr, props) {
     const indexes = {};
     props.forEach(prop => {
-      const minValueObj = arr.reduce((prev, current) => {
-        return (prev[prop] < current[prop]) ? prev : current;
-      });
-      indexes[prop] = arr.indexOf(minValueObj);
+        const minValueObj = arr.reduce((prev, current) => {
+            return (prev[prop] < current[prop]) ? prev : current;
+        });
+        indexes[prop] = arr.indexOf(minValueObj);
     });
     return indexes;
-  }
+}
 
 // Function to calculate months difference between two dates
 function monthDiff(d1, d2) {
@@ -197,26 +239,26 @@ function monthDiff(d1, d2) {
     months -= d1.getMonth();
     months += d2.getMonth();
     return months <= 0 ? 0 : months;
-  }
+}
+
+
 
 try {
-  existingMortgage = p1 ? JSON.parse(p1) : undefined;
-  marketRates = p2 ? JSON.parse(p2) : undefined;
-  marketPrime = p3 ? parseFloat(p3) : 6.5; // Assuming p3 is a number in string format.
-  marketRateLender = marketPrime; // Use marketPrime as the default if p3 is defined.
+    existingMortgage = p1 ? JSON.parse(p1) : undefined;
+    marketRates = p2 ? JSON.parse(p2) : undefined;
+    marketPrime = p3 ? parseFloat(p3) : 6.5; // Assuming p3 is a number in string format.
+    marketRateLender = marketPrime; // Use marketPrime as the default if p3 is defined.
 } catch (error) {
-  console.error("Error parsing JSON input:", error);
-  // Handle the error appropriately, perhaps by returning a message or setting default values.
+    console.error("Error parsing JSON input:", error);
+    // Handle the error appropriately, perhaps by returning a message or setting default values.
 }
 
 // Ensure that the required variables are defined before calling mortgageComparison
 if (existingMortgage && marketRates && typeof marketPrime === 'number') {
-  const comparisonResult = mortgageComparison(existingMortgage, marketRates, marketPrime, marketRateLender);
-  return JSON.stringify(comparisonResult).slice(1, -1).replace(/\\/g, "");
+    const comparisonResult = mortgageComparison(existingMortgage, marketRates, marketPrime, marketRateLender);
+    return JSON.stringify(comparisonResult).slice(1, -1).replace(/\\/g, "");
 } else {
-  // Handle the case where the inputs are not defined or valid
-  console.error("Invalid input for mortgage comparison.");
-  // Return an error message or handle it as per your application's error handling policy
+    // Handle the case where the inputs are not defined or valid
+    console.error("Invalid input for mortgage comparison.");
+    // Return an error message or handle it as per your application's error handling policy
 }
-
-return JSON.stringify(mortgageComparison(existingMortgage, marketRates, marketPrime, marketRateLender)).slice(1, -1).replace(/\\/g, "");
--- qualified_market_rates.js
+++ qualified_market_rates.js
@@ -1,36 +1,56 @@
-function filterRates(currentMortgage, marketRates, currentPrimeRate) {
-
-  let currentRate;  // Declare currentRate here
-
-  if (currentMortgage.interestTypeDd === "Fixed") {
-    currentRate = currentMortgage.requestedRate;
-  } else {
-    const discount =  currentMortgage.rate.discount || 0;
-    const premium = currentMortgage.rate.premium || 0;
-    currentRate = currentPrimeRate - discount + premium;
+// Function to calculate the current mortgage rate
+function calculateCurrentRate(mortgage, primeRate) {
+  if (mortgage.interestTypeDd === "Fixed") {
+      // For fixed rate, use the requested rate directly
+      return mortgage.requestedRate;
   }
-  
-  const maturityDate = currentMortgage.maturityDate;
-    
-  // Process and filter marketRates
-  marketRates = marketRates.map(rate => {
-    const match = rate.Term.match(/^(\d+) YR( VRM)?$/);
-    if (match) {
-      rate.termYears = parseInt(match[1]);
-      rate.type = match[2] ? "Variable" : "Fixed";
-    }
-    rate.Rate = parseFloat(rate.Rate);
-    rate.Lender = rate.Lender.replace(/\n/g, ' ');
-    return rate;
-  }).filter(rate => rate.termYears !== undefined && !isNaN(rate.Rate) && rate.Rate < currentRate);
-  
-    // Filter out rates that are closest to year_to_maturity 
-    return marketRates;
+  // For variable rate, calculate using prime rate, discount, and premium
+  const discount = mortgage.rateDiscount || 0;
+  const premium = mortgage.ratePremium || 0;
+  return primeRate - discount + premium;
 }
-  
-// Parse and call the function
+
+// Function to process and standardize each rate object
+function processRate(rate) {
+  // Check if rate.Term is a string before calling match to avoid TypeError
+  if (typeof rate.Term === 'string') {
+      // Use regex to match the Term format (e.g., "5 YR" or "5 YR VRM")
+      const termMatch = rate.Term.match(/^(\d+) YR( VRM)?$/);
+      if (termMatch) {
+          // If matched, assign termYears and type based on the regex groups
+          rate.termYears = parseInt(termMatch[1]);
+          rate.type = termMatch[2] ? "Variable" : "Fixed";
+      }
+  } else if (rate._TermMonths && rate.TermType) {
+      // Handle the case where Term is defined in months (_TermMonths)
+      // and the term type is specified in TermType
+      rate.termYears = rate._TermMonths / 12;
+      rate.type = rate.TermType;
+  }
+
+  // Convert the Rate to a float for consistent data type
+  rate.Rate = parseFloat(rate.Rate);
+  // Clean up the Lender string by replacing newline characters
+  rate.Lender = rate.Lender.replace(/\n/g, ' ');
+  return rate;
+}
+
+// Main function to filter market rates based on current mortgage data
+function filterRates(currentMortgage, marketRates, currentPrimeRate) {
+  // Calculate the current rate based on mortgage details
+  const currentRate = calculateCurrentRate(currentMortgage, currentPrimeRate);
+  // Process each rate in market rates and then filter
+  const processedRates = marketRates.map(processRate);
+  return processedRates.filter(rate => 
+      // Filter criteria: defined term years, rate not NaN, and less than current rate
+      rate.termYears !== undefined && !isNaN(rate.Rate) && rate.Rate < currentRate
+  );
+}
+
+// Parse inputs and call the function
 const marketRates = JSON.parse('[' + p3 + ']');
 const currentMortgage = JSON.parse(p1);
 const currentPrimeRate = p2;
 
+// Return the filtered rates as a JSON string
 return JSON.stringify(filterRates(currentMortgage, marketRates, currentPrimeRate));
